5793adc3c32829543be91e355d3fa1cea33e83fe
juck89724 <juck89724@gmail.com>
2019-12-31 20:54:33 +0800

    增加刷資料庫的地方

--- gantt/urls.py
+++ gantt/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:character_id>/', views.detail, name='detail'),
+    path('create',views.create_data,name='create')
 ]
